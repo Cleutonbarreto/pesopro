15bd724f97dc07c59b75089de0a32b218577a060
Cleuton Barreto <cleuton.barreto@gmail.com>
2024-09-29 23:31:48 -0300

    README

--- pesopro/src/main/java/com/pesopro/app/entities/Filme.java
+++ pesopro/src/main/java/com/pesopro/app/entities/Filme.java
@@ -1,4 +1,21 @@
 package com.pesopro.app.entities;
 
-public class Filme {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_filmes")
+public class Filme implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descrição;
 }
---
+++ pesopro/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_filmes (id, descrição) VALUES (1, 'Filme A');
+INSERT INTO tb_filmes (id, descrição) VALUES (2, 'Filme B');
+INSERT INTO tb_filmes (id, descrição) VALUES (3, 'Filme C');
